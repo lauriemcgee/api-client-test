7b06cc8e06fb6ba06da3691a9a84a83b0635ebba
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-06 19:40:47 -0500

    view and new added.  also added the show page without a button and I'm commiting it because I live life on the dangerous side-look out for my button commit next, guys

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,45 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Unirest.get("http://localhost:3000/api/v1/characters").body
+    render "index.html.erb"
+  end
+  def new
+    render "new.html.erb"
+  end
+  def create
+    @character = Unirest.post("http://localhost:3000/api/v1/characters", parameters[{
+        character_name: params["character_name"],
+        char_class: params["char_class"],
+        level: params["level"],
+        background: params["background"],
+        player_name: params["player_name"],
+        race: params["race"],
+        alignment: params["alignment"],
+        experience_points: params["experience_points"],
+        strength: params["strength"],
+        dexterity: params["dexterity"],
+        constitution: params["constitution"],
+        intelligence: params["intelligence"],
+        wisdom: params["wisdom"],
+        charisma: params["charisma"],
+        proficiency_bonus: params["proficiency_bonus"],
+        armor_class: params["armor_class"],
+        initiative: params["initiative"],
+        speed: params["speed"],
+        current_hit_points: params["current_hit_points"],
+        temporary_hit_points: params["temporary_hit_points"],
+        hit_dice: params["hit_dice"],
+        personality_traits: params["personality_traits"],
+        ideals: params["ideals"],
+        bonds: params["bonds"],
+        flaws: params["flaws"],
+        features_and_traits: params["features_and_traits"],
+        attacks_and_spellcasting: params["attacks_and_spellcasting"],
+        equipment: params["equipment"],
+        other_proficiencies_and_languages: params["other_proficiencies_and_languages"]
+      }]).body
+    redirect_to "/characters"
+  end
   def show
     @character = Unirest.get("http://localhost:3000/api/v1/characters/#{params[:id]}").body
     render "show.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get "/" => "characters#index"
+  get "/characters" => "characters#index"
+  get "/characters/new" => "characters#new"
+  post "/characters" => "characters#create"
   get "/characters/:id" => "characters#show"
 end
